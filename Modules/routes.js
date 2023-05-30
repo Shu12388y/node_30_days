38fee9c0ebb12c2464cd2841d71cffb1f7029975
Shu12388y <spl.sp999@gmail.com>
2023-05-30 20:26:32 +0530

    new files add of express

---
+++ Modules/routes.js
@@ -0,0 +1,6 @@
+
+const route=(req,res)=>{
+    res.write("<h1>Home is on</h1>")
+return res.end()
+}
+module.exports=route;
---
+++ npm modules/app.js
@@ -0,0 +1,8 @@
+//just console.log the content
+
+//add start key word in the package.json and 
+// "start":"node app.js"
+// using this command we can run the program 
+
+
+console.log("started")
