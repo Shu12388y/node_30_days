b358105d34ab5ca1cba95d4d4f272c06a3a96b5e
Shu12388y <spl.sp999@gmail.com>
2023-05-30 17:55:58 +0530

    new files

--- response/app.js
+++ response/app.js
@@ -1,14 +1,7 @@
 const http=require("http");
 const port=3000;
 const server =  http.createServer((req,res)=>{
-  const url=req.url;
-  if(url === "/"){
-    res.write("<html>")
-    res.write("<head><title>first server</title></head>")
-  res.write("<h1>server is on</h1>")
-    res.write("</html>")
-    
-    return res.end()  
-  }
+ res.write("<h1>webpage is on</h1>")
+ res.end()
 })
 server.listen();
