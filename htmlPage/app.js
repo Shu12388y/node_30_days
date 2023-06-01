a55ff66a9342cb887367e53a409de03951b011dc
Shu12388y <spl.sp999@gmail.com>
2023-06-01 18:40:09 +0530

    404 error file add

--- htmlPage/app.js
+++ htmlPage/app.js
@@ -5,7 +5,11 @@ const app = express()
 const server=http.createServer(app);
 const homeRoute=require("./routes/home")
 const productRoute=require("./routes/product")
+const path=require("path")
 
 app.use(homeRoute)
 app.use(productRoute)
+app.use("/",(req,res,next)=>{
+    res.status(404).sendFile(path.join(__dirname,"views","404.html"))
+})
 server.listen(port, () => console.log(`Example app listening on port ${port}!`))
